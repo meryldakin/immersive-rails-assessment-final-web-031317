9b43ca149817c6ee53c1b0fefd316012c6ead451
meryldakin <meryl.k.dakin@gmail.com>
2017-04-21 10:24:38 -0400

    finished

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -13,10 +13,26 @@ class AppearancesController < ApplicationController
     end
   end
 
+  def edit
+    unless @appearance = Appearance.find(params[:id])
+      redirect_to new_appearance_path
+    end
+  end
+
+  def update
+    @appearance = Appearance.find(params[:id])
+    @appearance.update(appearance_params)
+    if @appearance.save
+      redirect_to episode_path(@appearance.episode)
+    else
+      redirect_to edit_appearance(@appearance)
+    end
+  end
+
   private
 
   def appearance_params
-    params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+    params.require(:appearance).permit(:guest_id, :episode_id, :rating, :user_id)
   end
 
 end
